ec9522c5122716cd2c254573fe5d56422b204f2b
Rubén Zúñiga <ruben.zuniga@mi.unc.edu.ar>
2026-08-15 12:31:12 -0300

    test

--- TP5/com_serie_tp5.py
+++ TP5/com_serie_tp5.py
@@ -28,14 +28,15 @@ ser.timeout=None
 ser.flushInput()
 ser.flushOutput()
 print(ser.timeout)
+sys.set_int_max_str_digits(10000)
 
 ## Armar trama
 # cabecera = 10100001 = 161 = 0xA1
-cabecera = chr(161)
+cabecera = 0xA1
 # dispositivo: elijo el valor 233 = 0xfe
-dispositivo = chr(254)
+dispositivo = 0xFE
 # fin_de_trama = 01000001 = 65 = 0x41
-fin_de_trama = chr(65)
+fin_de_trama = 0x41
 
 # Mensaje de inicio
 print ('--- Comunicación con FPGA:',ser.port,'---\r\n')
@@ -64,19 +65,32 @@ while 1 :
                 time.sleep(1)
 
     elif data_write_str == 's':
+        # Limpiar buffer
+        # ser.read(ser.in_waiting)
         # Enviar dato
         print ("Wait Input Data")
-        trama = cabecera + dispositivo + data_write_str + fin_de_trama
-        ser.write(trama.encode())
+        trama = []
+        trama.append(cabecera)
+        trama.append(dispositivo)
+        trama.append(data_write_str.encode())
+        trama.append(fin_de_trama)
+        # trama = cabecera + dispositivo + data_write_str + fin_de_trama
+        print(bytearray(trama))
+        ser.write(cabecera.encode())
+        ser.write(dispositivo.encode())
+        ser.write(data_write_str.encode())
+        ser.write(fin_de_trama.encode())
 
         # Esperar y mostrar respuesta del receptor
         time.sleep(2)
-        data_read = ser.read(1)
-        # data_read = ser.read(ser.in_waiting)
-        data_read_str = str(int.from_bytes(data_read,byteorder='big'))
-        print(ser.inWaiting())
-        if data_read_str != '':
-            print (">>" + data_read_str)
+        while ser.in_waiting > 0:
+            data_read = ser.read(1)
+            # data_read = ser.read(ser.in_waiting)
+            data_read_str = str(int.from_bytes(data_read,byteorder='big'))
+            print(ser.inWaiting())
+            if data_read_str != '':
+                print (">>",data_read_str,"|",data_read)
+                print(len(data_read))
 
     elif data_write_str == 'exit':
         if ser.isOpen():
