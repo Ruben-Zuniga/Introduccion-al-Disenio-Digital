4d0ef3f3938294cb3925e21ab88b663e90f627da
Rubén Zúñiga <ruben.zuniga@mi.unc.edu.ar>
2026-08-22 15:50:44 -0300

    arreglado uart con 1 byte

--- TP5/com_serie_tp5.py
+++ TP5/com_serie_tp5.py
@@ -28,15 +28,15 @@ ser.timeout=None
 ser.flushInput()
 ser.flushOutput()
 print(ser.timeout)
-sys.set_int_max_str_digits(10000)
+# sys.set_int_max_str_digits(10000)
 
 ## Armar trama
 # cabecera = 10100001 = 161 = 0xA1
-cabecera = 0xA1
+cabecera = b'\xA1'
 # dispositivo: elijo el valor 254 = 0xfe
-dispositivo = 0xFE
+dispositivo = b'\xFE'
 # fin_de_trama = 01000001 = 65 = 0x41
-fin_de_trama = 0x41
+fin_de_trama = b'\x41'
 
 # Mensaje de inicio
 print ('--- Comunicación con FPGA:',ser.port,'---\r\n')
@@ -53,55 +53,69 @@ while 1 :
     if data_write_str == '0':
         while data_write_str != 'back':
             print('Indique LED a controlar [0/1/2/3] o escriba "back" para volver.')
-            data_write = input("<< ")
+            led = input("<< ")
             
             # Dato de 1 byte
-            data_write_str = str(data_write)
+            data_write_str = str(led)
 
             if data_write_str != 'back':
-                # # Limpiar buffer
-                # data_read = ser.read(ser.in_waiting)
-                # Armar y enviar trama
-                trama = [cabecera, dispositivo, data_write_str.encode(), fin_de_trama]
-                print(trama)
-                # for i in range(10):
-                ser.write(cabecera)
-                ser.write(dispositivo)
-                ser.write(data_write_str.encode())
-                ser.write(fin_de_trama)
+                print('Indique el color del LED [RGB]:')
+                print('- R: Rojo [0/1]')
+                print('- G: Verde [0/1]')
+                print('- B: Azul [0/1]')
+                color = input('<< ')
+                print(color)
+                color_str = str(color)
 
+                match color_str:
+                    case '000':
+                        data_write = 0
+                    case '100':
+                        data_write = 1
+                    case '010':
+                        data_write = 2
+                    case '110':
+                        data_write = 3
+                    case '001':
+                        data_write = 4
+                    case '101':
+                        data_write = 5
+                    case '011':
+                        data_write = 6
+                    case '111':
+                        data_write = 7
+                    case _:
+                        data_write = 0
+
+                data_write = data_write + 8*int(led)
+                data_write_str = data_write.to_bytes(1, 'big')
+                print(data_write, data_write_str)
+
+                # Armar y enviar trama
+                ser.write(data_write_str)
                 time.sleep(1)
 
     elif data_write_str == '1':
         print ("Wait Input Data")
-
-        # # Limpiar buffer
-        # data_read = ser.read(ser.in_waiting)
         
         # Armar y enviar trama
-        data_write_str = '4'
-        data_write = 4
-        # trama = [cabecera, dispositivo, data_write_str.encode(), fin_de_trama]
-        print(cabecera, dispositivo, data_write_str.encode(), fin_de_trama)
-        # for i in range(10):
+        data_write = 32
+        data_write_str = data_write.to_bytes(1, 'big')
+        print(data_write, data_write_str)
+        # print(cabecera, dispositivo, data_write_str.encode(), fin_de_trama)
         ser.reset_input_buffer()
-        # ser.write(cabecera)
-        # ser.write(dispositivo)
-        # ser.write(data_write_str.encode())
-        # ser.write(fin_de_trama)
-        # ser.write(struct.pack('BBBB', cabecera, dispositivo, data_write, fin_de_trama))
-        ser.write(bytearray([cabecera, dispositivo, data_write, fin_de_trama]))
+        ser.write(data_write_str)
 
         # Esperar y mostrar respuesta del receptor
-        time.sleep(1)
+        time.sleep(2)
+        print(ser.inWaiting())
         while ser.in_waiting > 0:
-            data_read = ser.read(4)
-            # print(data_read)
-            print(data_read[0], data_read[1], data_read[2], data_read[3])
-            # if data_read[0] == 161 and data_read[1] == 239 and data_read[3] == 65:
-            #     print (">>",data_read[2])
-            #     break
-            # time.sleep(0.5)
+            data_read = ser.read(1)
+            out = int.from_bytes(data_read,byteorder='big')
+
+            # if out != '':
+            print (">>",out,"(",ser.inWaiting(),")")
+
 
     elif data_write_str == 'exit':
         if ser.isOpen():
--- TP5/protocolo_main.c
+++ TP5/protocolo_main.c
@@ -1,18 +1,15 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <sys/_intsup.h>
 #include "xparameters.h"
 #include "xil_cache.h"
 #include "xgpio.h"
 #include "platform.h"
 #include "xuartlite.h"
-//#include "microblaze_sleep.h"
+#include "microblaze_sleep.h"
 
-// Fijarse: cada nueva version cambia de nombre los xparameters
-// Los dos puertos tienen la misma direccion porque es bidireccional (por control)
-#define PORT_IN	 		XPAR_AXI_GPIO_0_BASEADDR //XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
-#define PORT_OUT 		XPAR_AXI_GPIO_0_BASEADDR //XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
+#define PORT_IN	 		XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
+#define PORT_OUT 		XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
 
 //Device_ID Operaciones
 #define def_SOFT_RST            0
@@ -34,15 +31,11 @@ int main()
 {
 	init_platform();
 	int Status;
-	Status = XUartLite_Initialize(&uart_module, 0);
-	if(Status!=XST_SUCCESS){
-        return XST_FAILURE;
-    }
+	XUartLite_Initialize(&uart_module, 0);
 
 	GPO_Value=0x00000000;
 	GPO_Param=0x00000000;
-    unsigned int N_bytes = 4;
-	unsigned char trama[N_bytes];
+	unsigned char cabecera[4];
 
 	Status=XGpio_Initialize(&GpioInput, PORT_IN);
 	if(Status!=XST_SUCCESS){
@@ -52,106 +45,45 @@ int main()
 	if(Status!=XST_SUCCESS){
 		return XST_FAILURE;
 	}
-
-    // --Setear puerto de entrada y de salida--
-    // Todos de salida
 	XGpio_SetDataDirection(&GpioOutput, 1, 0x00000000);
-    // Todos de entrada
 	XGpio_SetDataDirection(&GpioInput, 1, 0xFFFFFFFF);
 
-    // Datos de la trama
-    unsigned char cabecera = (unsigned char)161; // 0xA1
-    unsigned char dispositivo = (unsigned char)239; // 0xEF
-    unsigned char fin_de_trama = (unsigned char)65; // 0x41
-
 	u32 value;
-    unsigned char datos[N_bytes];
-    datos[0] = cabecera;
-    datos[1] = dispositivo;
-    datos[3] = fin_de_trama;
-
+  unsigned char datos;
 	while(1){
-    
+    read(stdin,&cabecera[0],1);
+
+    /* unsigned char datos_rec[3]; */
+    /* short int i; */
+    /* for(i=0;i<3;i++){ */
+    /*   datos_rec[i]=XUartLite_RecvByte((&uart_module)->RegBaseAddress); */
+    /* } */
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 // ACA es donde se escribe toda la funcionalidad
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-        // Sobreescribir el primer byte de la trama 'trama' por el byte recibido
-        XUartLite_Recv(&uart_module, &trama[0],  1);
-        if(trama[0] == (unsigned char)161){
-            XUartLite_Recv(&uart_module, &trama[1],  1);
-            if(trama[1] == (unsigned char)254){
-                XUartLite_Recv(&uart_module, &trama[2],  1);
-
-                // Resetear el buffer del UART                
-                // XUartLite_ResetFifos(&uart_module);
-
-                switch(trama[2]){
-                case '0':
-                    XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000249);
-                    break;
-                case '1':
-                    XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000492);
-                    break;
-                case '2':
-                    XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000924);
-                    break;
-                case '3':
-                    XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000000);
-                    break;
-                case '4':
-                    // Leer los 4 switches
-                    value = XGpio_DiscreteRead(&GpioInput, 1);
-                    datos[2]=(char)(value & (0x0000000F));
-                    // Esperar que deje de transmitir
-                    while(XUartLite_IsSending(&uart_module)){}
-                    // Enviar por UART el valor leido
-                    XUartLite_Send(&uart_module, &datos[0],N_bytes);
-                    break;
-                }
-            }
-        }
-
-        // Loopback
-        // XUartLite_Recv(&uart_module, &trama[0],  1);
-        // XUartLite_Recv(&uart_module, &trama[1],  1);
-        // XUartLite_Recv(&uart_module, &trama[2],  1);
-        // XUartLite_Recv(&uart_module, &trama[3],  1);
-        XUartLite_Recv(&uart_module, &trama[0],  N_bytes);
-
-        if(trama[0] == (unsigned char)161 || trama[1] == (unsigned char)254 || trama[3] == (unsigned char)65){
-            datos[0] = trama[0];
-            datos[1] = trama[1];
-            datos[2] = trama[2];
-            datos[3] = trama[3];
-
-            // Esperar que deje de transmitir
-            while(XUartLite_IsSending(&uart_module)){}
-            // Enviar por UART el valor leido
-            // XUartLite_Send(&uart_module, &datos[0],1);
-            // XUartLite_Send(&uart_module, &datos[1],1);
-            // XUartLite_Send(&uart_module, &datos[2],1);
-            // XUartLite_Send(&uart_module, &datos[3],1);
-            XUartLite_Send(&uart_module, &datos[0],N_bytes);
-        }
-
-        // if (trama[0] == '4') {
-        //     datos = (char)trama[0];
-            
-        //     unsigned char cabecera = (unsigned char)161; // 0xA1
-        //     unsigned char dispositivo = (unsigned char)239; // 0xEF
-        //     unsigned char fin_de_trama = (unsigned char)65; // 0x41
-        //     XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000249);
-        //     XUartLite_Send(&uart_module, &cabecera,1);
-        //     XUartLite_Send(&uart_module, &dispositivo,1);
-        //     XUartLite_Send(&uart_module, &datos,1);
-        //     XUartLite_Send(&uart_module, &fin_de_trama,1);
-        // }
-        
+		   switch(cabecera[0]){
+           case '0':
+               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000249);
+               break;
+           case '1':
+               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000492);
+               break;
+           case '2':
+               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000924);
+               break;
+           case '3':
+               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000000);
+               value = XGpio_DiscreteRead(&GpioInput, 1);
+               datos=(char)(value&(0x0000000F));
+               while(XUartLite_IsSending(&uart_module)){}
+               XUartLite_Send(&uart_module, &(datos),1);
+               break;
+		   }
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 // FIN de toda la funcionalidad
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    }
+        }
 	
 	cleanup_platform();
 	return 0;
