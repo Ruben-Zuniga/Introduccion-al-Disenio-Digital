2a35518ced225d29841684307f9eb52b1300113f
Rubén Zúñiga <ruben.zuniga@mi.unc.edu.ar>
2026-08-22 17:34:19 -0300

    completado envio y recepcion con 4 bytes

--- TP5/com_serie_tp5.py
+++ TP5/com_serie_tp5.py
@@ -33,10 +33,17 @@ print(ser.timeout)
 ## Armar trama
 # cabecera = 10100001 = 161 = 0xA1
 cabecera = b'\xA1'
-# dispositivo: elijo el valor 254 = 0xfe
+# dispositivo (FPGA): elijo el valor 254 = 0xfe
 dispositivo = b'\xFE'
 # fin_de_trama = 01000001 = 65 = 0x41
 fin_de_trama = b'\x41'
+
+# Inicio de trama
+init = (int.from_bytes(cabecera,byteorder='big') & 0xF0) >> 4
+
+# Este dispositivo (Python)
+dispositivo_host = b'\xEF'
+dispositivo_host = int.from_bytes(dispositivo_host,byteorder='big')
 
 # Mensaje de inicio
 print ('--- Comunicación con FPGA:',ser.port,'---\r\n')
@@ -58,13 +65,17 @@ while 1 :
             # Dato de 1 byte
             data_write_str = str(led)
 
+            # print(int(data_write))
+
             if data_write_str != 'back':
                 print('Indique el color del LED [RGB]:')
                 print('- R: Rojo [0/1]')
                 print('- G: Verde [0/1]')
                 print('- B: Azul [0/1]')
                 color = input('<< ')
-                print(color)
+
+                # print(color)
+
                 color_str = str(color)
 
                 match color_str:
@@ -89,33 +100,59 @@ while 1 :
 
                 data_write = data_write + 8*int(led)
                 data_write_str = data_write.to_bytes(1, 'big')
-                print(data_write, data_write_str)
+
+                # print(data_write)
+                # print(cabecera + dispositivo + data_write_str + fin_de_trama)
 
                 # Armar y enviar trama
-                ser.write(data_write_str)
+                ser.write(cabecera + dispositivo + data_write_str + fin_de_trama)
                 time.sleep(1)
 
     elif data_write_str == '1':
-        print ("Wait Input Data")
+        print ("Esperando respuesta...")
         
         # Armar y enviar trama
         data_write = 32
         data_write_str = data_write.to_bytes(1, 'big')
-        print(data_write, data_write_str)
-        # print(cabecera, dispositivo, data_write_str.encode(), fin_de_trama)
-        ser.reset_input_buffer()
-        ser.write(data_write_str)
+        
+        # print(cabecera + dispositivo + data_write_str + fin_de_trama)
 
+        # Reiniciar buffer de recepcion
+        ser.reset_input_buffer()
+        # Enviar dato
+        ser.write(cabecera + dispositivo + data_write_str + fin_de_trama)
         # Esperar y mostrar respuesta del receptor
         time.sleep(2)
-        print(ser.inWaiting())
+
+        # print(ser.inWaiting())
+
         while ser.in_waiting > 0:
-            data_read = ser.read(1)
-            out = int.from_bytes(data_read,byteorder='big')
+            # Leer cabecera y dispositivo
+            data_read = ser.read(2)
 
-            # if out != '':
-            print (">>",out,"(",ser.inWaiting(),")")
+            # Separar cabecera, tamaño y dispositivo
+            data_read_int = int.from_bytes(data_read,byteorder='big')
+            init_read = (data_read_int & 0xF000) >> 12
+            size_read = (data_read_int & 0x0F00) >> 8
+            device_read = data_read_int & 0x00FF
 
+            # print(data_read, init_read, size_read, device_read)
+            # print(init, dispositivo_host)
+
+            # Comparar Cabecera y dispositivo
+            if init_read == init and device_read == dispositivo_host:
+                # Leer tamaño indicado por la trama
+                data_read = ser.read(size_read)
+                # Convertir a entero e imprimir dato recibido
+                data_read_int = int.from_bytes(data_read,byteorder='big')
+                print (">>",data_read_int)
+
+                # print (">>",data_read_int,"(",ser.inWaiting(),")")
+
+                # Leer fin de trama
+                data_read = ser.read(1)
+
+                # print(data_read)
 
     elif data_write_str == 'exit':
         if ser.isOpen():
--- TP5/protocolo_main.c
+++ TP5/protocolo_main.c
@@ -1,15 +1,15 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <sys/_intsup.h>
 #include "xparameters.h"
 #include "xil_cache.h"
 #include "xgpio.h"
 #include "platform.h"
 #include "xuartlite.h"
-#include "microblaze_sleep.h"
 
-#define PORT_IN	 		XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
-#define PORT_OUT 		XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
+#define PORT_IN	 		XPAR_AXI_GPIO_0_BASEADDR //XPAR_GPIO_0_DEVICE_ID
+#define PORT_OUT 		XPAR_AXI_GPIO_0_BASEADDR //XPAR_GPIO_0_DEVICE_ID
 
 //Device_ID Operaciones
 #define def_SOFT_RST            0
@@ -29,61 +29,71 @@ XUartLite uart_module;
 
 int main()
 {
-	init_platform();
-	int Status;
-	XUartLite_Initialize(&uart_module, 0);
+    init_platform();
+    int Status;
+    XUartLite_Initialize(&uart_module, 0);
 
-	GPO_Value=0x00000000;
-	GPO_Param=0x00000000;
-	unsigned char cabecera[4];
+    GPO_Value=0x00000000;
+    GPO_Param=0x00000000;
 
-	Status=XGpio_Initialize(&GpioInput, PORT_IN);
-	if(Status!=XST_SUCCESS){
+    Status=XGpio_Initialize(&GpioInput, PORT_IN);
+    if(Status!=XST_SUCCESS){
         return XST_FAILURE;
     }
-	Status=XGpio_Initialize(&GpioOutput, PORT_OUT);
-	if(Status!=XST_SUCCESS){
-		return XST_FAILURE;
-	}
-	XGpio_SetDataDirection(&GpioOutput, 1, 0x00000000);
-	XGpio_SetDataDirection(&GpioInput, 1, 0xFFFFFFFF);
+    Status=XGpio_Initialize(&GpioOutput, PORT_OUT);
+    if(Status!=XST_SUCCESS){
+        return XST_FAILURE;
+    }
+    XGpio_SetDataDirection(&GpioOutput, 1, 0x00000000);
+    XGpio_SetDataDirection(&GpioInput, 1, 0xFFFFFFFF);
 
-	u32 value;
-  unsigned char datos;
+    u32 value;
+    u32 recv_count = 0;
+
+    u8 trama[4] = {0};
+    u8 datos[4];
+    u8 led;
+    u8 color;
+    u8 cabecera = 0xA1;
+    u8 dispositivo = 0xEF;
+    u8 fin_de_trama = 0x41;
+
+    datos[0] = cabecera;
+    datos[1] = dispositivo;
+    datos[3] = fin_de_trama;
+
 	while(1){
-    read(stdin,&cabecera[0],1);
+        // Entrar en bucle hasta leer 4 bytes (el UART a veces recibe con delay)
+        while(recv_count != 4){
+            recv_count += XUartLite_Recv(&uart_module,
+                                         &trama[0] + recv_count,
+                                         4 - recv_count);
+        }
+        recv_count = 0;
 
-    /* unsigned char datos_rec[3]; */
-    /* short int i; */
-    /* for(i=0;i<3;i++){ */
-    /*   datos_rec[i]=XUartLite_RecvByte((&uart_module)->RegBaseAddress); */
-    /* } */
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// ACA es donde se escribe toda la funcionalidad
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        // Comparar cabecera, dispositivo y fin de trama
+        if(trama[0] == 0xA1 && trama[1] == 0xFE && trama[3] == 0x41){
+            if(trama[2] == 32){
+                // Leer switches
+                value = XGpio_DiscreteRead(&GpioInput, 1);
+                datos[2] = (char)(value & 0x0000000F);
 
-		   switch(cabecera[0]){
-           case '0':
-               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000249);
-               break;
-           case '1':
-               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000492);
-               break;
-           case '2':
-               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000924);
-               break;
-           case '3':
-               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000000);
-               value = XGpio_DiscreteRead(&GpioInput, 1);
-               datos=(char)(value&(0x0000000F));
-               while(XUartLite_IsSending(&uart_module)){}
-               XUartLite_Send(&uart_module, &(datos),1);
-               break;
-		   }
+                XUartLite_Send(&uart_module, &datos[0],4);
+                while(XUartLite_IsSending(&uart_module)){}
+            }
+            else{
+                // Cambiar LED
+                led = trama[2] >> 3;
+                color = trama[2] & 0x07;
+                XGpio_DiscreteWrite(&GpioOutput,1, (u32)color << (led * 3));
+            }
+        }
+
+
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 // FIN de toda la funcionalidad
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        }
+    }
 	
 	cleanup_platform();
 	return 0;
