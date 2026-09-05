ae17c6d4c7ec2963481ddacb22028f2c09ec538a
Rubén Zúñiga <ruben.zuniga@mi.unc.edu.ar>
2026-09-05 19:44:35 -0300

    agregado opcion de leer memoria por uart

--- TP6/com_serie_tp6.py
+++ TP6/com_serie_tp6.py
@@ -1,7 +1,7 @@
 import time
 import serial
 import sys
-import struct
+import matplotlib.pyplot as plt
 #############################################
 # Nota:
 # Comentar esta linea si se utiliza el puerto serie
@@ -44,6 +44,9 @@ init = (int.from_bytes(cabecera,byteorder='big') & 0xF0) >> 4
 dispositivo_host = b'\xEF'
 dispositivo_host = int.from_bytes(dispositivo_host,byteorder='big')
 
+# Memoria del DSP
+LOG_SIZE = 200
+
 # Mensaje de inicio
 print ('--- Comunicación con FPGA:',ser.port,'---\r\n')
 
@@ -52,7 +55,10 @@ while 1 :
     char_v = []
 
     # Mensaje a enviar
-    print('Envíe algo.')
+    print('Indique leer datos:')
+    print('\t1: Leer BER')
+    print('\t2: Obtener log de datos')
+
     data_write = input("<< ")
     data_write_str = str(data_write)
 
@@ -64,21 +70,103 @@ while 1 :
     print ("Esperando respuesta...")
         
     # Armar y enviar trama
-    data_write = int(data_write)
-    data_write_byte = data_write.to_bytes(1, 'big')
+    data_write_int = int(data_write)
+    data_write_byte = data_write_int.to_bytes(1, 'big')
 
     # Reiniciar buffer de recepcion
     ser.reset_input_buffer()
     # Enviar dato
     ser.write(cabecera + dispositivo + data_write_byte + fin_de_trama)
     print(cabecera + dispositivo + data_write_byte + fin_de_trama)
-    # Esperar y mostrar respuesta del receptor
-    time.sleep(2)
 
-    print('Esperando:', ser.inWaiting())
+    # Leer BER
+    if data_write_int == 1:
+        # Esperar y mostrar respuesta del receptor
+        time.sleep(2)
 
-    while ser.in_waiting > 0:
-        data_read = ser.read(4)
-        # Convertir a entero e imprimir dato recibido
-        data_read_int = int.from_bytes(data_read,byteorder='big')
-        print (">>",data_read, data_read_int)
+        print('Bytes entrantes:', ser.inWaiting())
+
+        # ber_values: valores de error y simbolos totales
+        # ber_values[0]: errores I parte alta
+        # ber_values[1]: errores I parte baja
+        # ber_values[2]: simbolos I parte alta
+        # ber_values[3]: simbolos I parte baja
+        # ber_values[4]: errores Q parte alta
+        # ber_values[5]: errores Q parte baja
+        # ber_values[6]: simbolos Q parte alta
+        # ber_values[7]: simbolos Q parte baja
+        ber_values = []
+
+        while ser.in_waiting > 0:
+            # Leer dato
+            data_read = ser.read(4)
+            # Convertir a entero e imprimir dato recibido
+            data_read_int = int.from_bytes(data_read,byteorder='big')
+            # print (">>", data_read_int)
+            ber_values.append(data_read_int)
+
+        # errores/simbolos = (parte_alta * 2**32 + parte_baja)
+        print (">> BER I:", (ber_values[0] * 2**32 + ber_values[1]) / \
+                            (ber_values[2] * 2**32 + ber_values[3]))
+        print (">> BER Q:", (ber_values[4] * 2**32 + ber_values[5]) / \
+                            (ber_values[6] * 2**32 + ber_values[7]))
+
+    elif data_write_int == 2:
+        # Esperar y mostrar respuesta del receptor
+        # time.sleep(2)
+
+        print('Bytes entrantes:', ser.inWaiting())
+
+        idx_read = 0
+        log_i = []
+        log_q = []
+
+        waiting_total = ser.inWaiting()
+
+        while idx_read < 2 * LOG_SIZE:
+            print('idx:', idx_read, ' - Bytes entrantes:', ser.inWaiting())
+
+            data_read = ser.read(1)
+            # Convertir a entero y guardar en arreglo
+            data_read_int = int.from_bytes(data_read,byteorder='big',signed=True)
+            # print (">>", data_read_int)
+            if idx_read < LOG_SIZE:
+                log_i.append(data_read_int)
+            else:
+                log_q.append(data_read_int)
+
+            # time.sleep(0.5)
+            idx_read = idx_read + 1
+            
+        plt.figure(figsize=[14,7])
+        plt.plot(log_i, 'ro-' , linewidth=1.5, label='Canal I')
+        plt.plot(log_q, 'bo-' , linewidth=1.5, label='Canal Q')
+
+        plt.legend()
+        plt.grid(True)
+        plt.title('Plot')
+        plt.xlabel('Muestras')
+        plt.ylabel('Amplitud')
+        plt.show()
+
+        # print(">> Datos I:")
+        # for i in range(LOG_SIZE):
+        #     print(log_i[i])
+
+        # print(">> Datos Q:")
+        # for i in range(LOG_SIZE):
+        #     print(log_q[i])
+
+    else:
+        print('Comando desconocido.')
+
+    # # Esperar y mostrar respuesta del receptor
+    # time.sleep(2)
+
+    # print('Esperando:', ser.inWaiting())
+
+    # while ser.in_waiting > 0:
+    #     data_read = ser.read(4)
+    #     # Convertir a entero e imprimir dato recibido
+    #     data_read_int = int.from_bytes(data_read,byteorder='big')
+    #     print (">>", data_read_int)
--- TP6/main.c
+++ TP6/main.c
@@ -16,6 +16,9 @@
 #define def_LOG_RUN             2
 #define def_LOG_READ            3
 
+// Memoria del log del DSP
+#define LOG_SIZE 1024
+
 XGpio GpioOutput;
 XGpio GpioParameter;
 XGpio GpioInput;
@@ -25,12 +28,18 @@ XUartLite uart_module;
 
 // Tramas del UART
 u8 frame_in[4] = {0};
-u8 frame_out[4] = {0};
+u8 frame_out_ber[4] = {0};
+u8 frame_out_log[2 * LOG_SIZE] = {0};
+
+// Log del DSP
+u32 log_mem_temp = 0;
+u8 log_mem_i[LOG_SIZE] = {0};
+u8 log_mem_q[LOG_SIZE] = {0};
 
 //Funcion para recibir 1 byte bloqueante
 //XUartLite_RecvByte((&uart_module)->RegBaseAddress)
 
-u32 get_gpio(u32 input)
+u32 write_and_read_gpio(u32 input)
 {
     XGpio_DiscreteWrite(&GpioOutput, 1, input);
     XGpio_DiscreteWrite(&GpioOutput, 1, input | (u32)(1 << 23));
@@ -39,14 +48,21 @@ u32 get_gpio(u32 input)
     return XGpio_DiscreteRead(&GpioOutput, 1);
 }
 
-void send_frame(u32 input)
+u32 write_gpio(u32 input)
 {
-    frame_out[0] = (input >> 24) & 0x000000FF;
-    frame_out[1] = (input >> 16) & 0x000000FF;
-    frame_out[2] = (input >>  8) & 0x000000FF;
-    frame_out[3] = (input >>  0) & 0x000000FF;
+    XGpio_DiscreteWrite(&GpioOutput, 1, input);
+    XGpio_DiscreteWrite(&GpioOutput, 1, input | (u32)(1 << 23));
+    XGpio_DiscreteWrite(&GpioOutput, 1, input);
+}
 
-    XUartLite_Send(&uart_module, &frame_out[0], 4);
+void send_frame_ber(u32 input)
+{
+    frame_out_ber[0] = (input >> 24) & 0x000000FF;
+    frame_out_ber[1] = (input >> 16) & 0x000000FF;
+    frame_out_ber[2] = (input >>  8) & 0x000000FF;
+    frame_out_ber[3] = (input >>  0) & 0x000000FF;
+
+    XUartLite_Send(&uart_module, &frame_out_ber[0], 4);
     while(XUartLite_IsSending(&uart_module)){}
 }
 
@@ -71,9 +87,9 @@ int main()
     XGpio_SetDataDirection(&GpioInput, 1, 0xFFFFFFFF);
     
     // Prender transceptor
-    XGpio_DiscreteWrite(&GpioOutput, 1, 0x0E000000);
-    XGpio_DiscreteWrite(&GpioOutput, 1, 0x0E800000);
-    XGpio_DiscreteWrite(&GpioOutput, 1, 0x0E000000);
+    XGpio_DiscreteWrite(&GpioOutput, 1, 0x0E000002);
+    XGpio_DiscreteWrite(&GpioOutput, 1, 0x0E800002);
+    XGpio_DiscreteWrite(&GpioOutput, 1, 0x0E000002);
 
     u32 error_i_high = 0;
     u32 error_i_low = 0;
@@ -95,6 +111,61 @@ int main()
         }
         recv_count = 0;
 
+        // Comparar cabecera, dispositivo y fin de trama
+        if(frame_in[0] == 0xA1 && frame_in[1] == 0xFE && frame_in[3] == 0x41){
+            
+            if(frame_in[2] == 1){
+
+                // Extraer BER - no se latchean los registros al mismo tiempo -> hay una diferencia de 207
+                //  simbolos entre Q e I
+                error_i_high = write_and_read_gpio(0x40000000);
+                error_i_low  = write_and_read_gpio(0x40000001);
+                symb_i_high  = write_and_read_gpio(0x40000002);
+                symb_i_low   = write_and_read_gpio(0x40000003);
+                error_q_high = write_and_read_gpio(0x40000004);
+                error_q_low  = write_and_read_gpio(0x40000005);
+                symb_q_high  = write_and_read_gpio(0x40000006);
+                symb_q_low   = write_and_read_gpio(0x40000007);
+
+                // Enviar todas las tramas
+                send_frame_ber(error_i_high);
+                send_frame_ber(error_i_low );
+                send_frame_ber(symb_i_high );
+                send_frame_ber(symb_i_low  );
+                send_frame_ber(error_q_high);
+                send_frame_ber(error_q_low );
+                send_frame_ber(symb_q_high );
+                send_frame_ber(symb_q_low  );
+            }
+            else if (frame_in[2] == 2){
+
+                // Comenzar logueo
+                write_gpio(0x10000000);
+                // Esperar si el MSB de la respuesta (bit de memoria llena) esta en 0
+                while (!(write_and_read_gpio(0x20000000) & 0x80000000)) {}
+                // Guardar datos
+                for (u32 i = 0; i < LOG_SIZE; i = i + 1) {
+                    log_mem_temp = write_and_read_gpio(0x20000000 | i);
+                    log_mem_i[i] = log_mem_temp & 0x000000FF;
+                    log_mem_q[i] = (log_mem_temp >> 16) & 0x000000FF;
+                }
+                // Enviar datos
+                for (u32 i = 0; i < LOG_SIZE; i = i + 1) {
+                    frame_out_log[i] = log_mem_i[i];
+                }
+                for (u32 i = 0; i < LOG_SIZE; i = i + 1) {
+                    frame_out_log[i + LOG_SIZE] = log_mem_q[i];
+                }
+                for (u32 i = 0; i < 2*LOG_SIZE / 32; i = i + 1) {
+                    XUartLite_Send(&uart_module, &frame_out_log[i], 32);
+                    while(XUartLite_IsSending(&uart_module)){}
+                    // Tiempo de espera para no sobrecargar el buffer
+                    for (u32 j = 0; j < 20000; j = j + 1) {}
+                }
+            }
+        }
+
+
         // // Loopback
         // for (int i = 0; i < 4; i++) {
         //     frame_out[i] = frame_in[i];
@@ -102,28 +173,6 @@ int main()
         // XUartLite_Send(&uart_module, &frame_out[0], 4);
         // while(XUartLite_IsSending(&uart_module)){}
 
-        // Extraer BER - no se latchean los registros al mismo tiempo -> hay una diferencia de 207
-        //  simbolos entre Q e I
-        error_i_high = get_gpio(0x40000000);
-        error_i_low = get_gpio(0x40000001);
-        symb_i_high = get_gpio(0x40000002);
-        symb_i_low = get_gpio(0x40000003);
-        error_q_high = get_gpio(0x40000004);
-        error_q_low = get_gpio(0x40000005);
-        symb_q_high = get_gpio(0x40000006);
-        symb_q_low = get_gpio(0x40000007);
-
-        // Enviar todas las tramas
-        send_frame(error_i_high);
-        send_frame(error_i_low);
-        send_frame(symb_i_high);
-        send_frame(symb_i_low);
-        send_frame(error_q_high);
-        send_frame(error_q_low);
-        send_frame(symb_q_high);
-        send_frame(symb_q_low);
-
-        
     }
 	
 	cleanup_platform();
