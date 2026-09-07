c4ba59b7a13aca967ca5c9bf495c17b6e0c2b7f1
Rubén Zúñiga <ruben.zuniga@mi.unc.edu.ar>
2026-09-07 17:34:52 -0300

    agregado protocolo en toggles y reset

--- TP6/com_serie_tp6.py
+++ TP6/com_serie_tp6.py
@@ -30,24 +30,84 @@ ser.flushOutput()
 print(ser.timeout)
 
 ## Armar trama
+# Inicio de trama
+init = 0x5 # primeros 3 bits '101'
 # cabecera = 10100001 = 161 = 0xA1
 cabecera = b'\xA1'
 # dispositivo (FPGA): elijo el valor 254 = 0xfe
 dispositivo = b'\xFE'
 # fin_de_trama = 01000001 = 65 = 0x41
 fin_de_trama = b'\x41'
+end = 0x2 # primeros 3 bits '010'
 
-# Inicio de trama
-init = (int.from_bytes(cabecera,byteorder='big') & 0xF0) >> 4
 # Este dispositivo (Python)
 dispositivo_host = b'\xEF'
-dispositivo_host = int.from_bytes(dispositivo_host,byteorder='big')
+# dispositivo_host = int.from_bytes(dispositivo_host,byteorder='big')
 
 # Memoria del DSP
 LOG_SIZE = 1024
 
 # Estado del RX
 rx_state = False
+
+def read_frame():
+    # Leer cabecera y tamaño
+    data_read = ser.read(1)
+
+    # Separar cabecera y tamaño
+    data_read_int = int.from_bytes(data_read,byteorder='big')
+    init_read = (data_read_int & 0xE0) >> 5
+    size_bit_read = (data_read_int & 0x10) >> 4
+
+    print('data read',data_read)
+    print('init read',init_read)
+    print('size_bit_read',size_bit_read)
+
+    if init_read == init:
+        # Tamaño corto
+        if size_bit_read == 0:
+            size_read = data_read_int & 0x0F
+        # Tamaño largo
+        else:
+            data_read = ser.read(2)
+            size_read = int.from_bytes(data_read,byteorder='big')
+
+        print('size_read',size_read)
+
+        # Leer dispositivo
+        data_read = ser.read(1)
+
+        print('data_read',data_read)
+
+        if data_read == dispositivo_host:
+
+            # Leer tamaño indicado por la trama
+            idx_read = 0
+            payload = []
+            while idx_read < size_read:
+                data_read = ser.read(1)
+                # Convertir a entero y guardar dato recibido en arreglo
+                payload.append(int.from_bytes(data_read,byteorder='big'))
+                print (">>", idx_read, payload[idx_read])
+                idx_read = idx_read + 1
+
+            # print (">>",data_read_int,"(",ser.inWaiting(),")")
+
+            # Comprobar fin de trama
+            data_read = ser.read(1)
+            print('data_read',data_read)
+            data_read_int = int.from_bytes(data_read,byteorder='big')
+            end_read = (data_read_int & 0xE0) >> 5
+            size_bit_check = (data_read_int & 0x10) >> 4
+            size_check = data_read_int & 0x0F
+
+            print('end_read',end_read)
+            print('size_bit_check',size_bit_check)
+
+            if end_read != end or size_bit_check != size_bit_read or size_check != size_read:
+                print('Advertencia: es posible que la trama este corrupta.')
+
+    return payload, size_read
 
 # Mensaje de inicio
 print ('--- Comunicación con FPGA:',ser.port,'---\r\n')
@@ -84,6 +144,7 @@ while 1 :
             data_write = input("<< ")
             # enviar el valor "{3,fase}"
             data_write = '3' + str(data_write)
+        rx_state = not rx_state
 
     # Armar y enviar trama
     data_write_int = int(data_write)
@@ -97,63 +158,41 @@ while 1 :
 
     # Resetear
     if data_write_int == 1:
-        
-        print('Bytes entrantes:', ser.in_waiting)
-        idx_read = 0
 
-        print('Bytes entrantes:', ser.in_waiting)
         # Leer dato
-        data_read = ser.read(1)
-        # Convertir a entero e imprimir dato recibido
-        data_read_int = int.from_bytes(data_read,byteorder='big')
-        print (">>", data_read)
-        if data_read_int != 0:
+        data_read_int, size_read = read_frame()
+            
+        if data_read_int[0] != 0:
             print('>> DSP reiniciado con exito.')
         else:
             print('>> Error: El DSP no se reinicio.')
 
-        idx_read = idx_read + 1
-
     # Togglear TX
     elif data_write_int == 2:
-        
-        print('Bytes entrantes:', ser.in_waiting)
-        
+
         # Leer dato
-        data_read = ser.read(1)
-        # Convertir a entero e imprimir dato recibido
-        data_read_int = int.from_bytes(data_read,byteorder='big')
-        print (">>", data_read)
-        if data_read_int:
+        data_read_int, size_read = read_frame()
+
+        if data_read_int[0] != 0:
             print('>> TX encendido.')
         else:
             print('>> TX apagado.')
 
     # Togglear RX
     elif data_write_int // 10 == 3:
-            
-        print('Bytes entrantes:', ser.in_waiting)
         
         # Leer dato
-        data_read = ser.read(1)
-        # Convertir a entero e imprimir dato recibido
-        data_read_int = int.from_bytes(data_read,byteorder='big')
-        print (">>", data_read)
-        if data_read_int:
+        data_read_int, size_read = read_frame()
+        
+        if data_read_int[0] != 0:
             print('>> RX encendido con fase', data_write_int % 3)
         else:
             print('>> RX apagado.')
-        rx_state = not rx_state
     
     # Leer BER
     elif data_write_int == 4:
-
-        print ("Esperando respuesta...")
-
         # Esperar y mostrar respuesta del receptor
         # time.sleep(2)
-
-        print('Bytes entrantes:', ser.in_waiting)
 
         idx_read = 0
         # ber_values: valores de error y simbolos totales
@@ -194,12 +233,6 @@ while 1 :
 
     # Leer memoria
     elif data_write_int == 5:
-
-        print ("Esperando respuesta...")
-        # Esperar y mostrar respuesta del receptor
-        # time.sleep(2)
-
-        print('Bytes entrantes:', ser.in_waiting)
 
         idx_read = 0
         log_i = []
